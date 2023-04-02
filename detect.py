fba0a9552713b2eea3fd8082926916b68854d4e4
xmkzbgx <74409174+xmkzbgx@users.noreply.github.com>
2023-04-02 22:17:28 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -149,8 +149,8 @@ def detect(save_img=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp54/weights/best.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='data/images/3.jpg', help='source')  # file/folder, 0 for webcam
-    # parser.add_argument('--source', type=str, default='0', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str, default='data/images/3.jpg', help='source')  #直接检测图片
+    # parser.add_argument('--source', type=str, default='0', help='source')  #调用摄像头
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
